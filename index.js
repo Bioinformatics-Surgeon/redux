5003e37042041ec07ec3ce0120072f4267a9424b
Cody Goss <c.ward.goss@gmail.com>
2019-07-26 22:23:18 -0800

    adds info

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
+// Actions DESCRIBE! the only things that can change the state in the store
+// - we only have to specifiy the actions TYPE; everything else is whatever we want
+
+const actionAddTodo = {
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false
+    }
+};
+
+const actionRemoveTodo = {
+    type: 'REMOVE_TODO',
+    id: 0
+};
+const actionToggleTodo = {
+    type: 'TOGGLE_TODO',
+    id: 0
+};
+
+const actionAddGoal = {
+    type: 'ADD_GOAL',
+    goal: {
+        id: 0,
+        name: 'Run a Marathon'
+    }
+};
+const actionRemoveGoal = {
+    type: 'REMOVE_GOAL',
+    id: 0
+};
+const actionToggleGoal = {
+    type: 'TOGGLE_GOAL',
+    id: 0
+};
+
 function createStore() {
-  // The Store should have four parts:
-  // 1. The state
-  // Public facing API for our Store is the following:
-  // you can interact with the state by involing on of the following methods below
-  // 2. Get state.
-  // 3. Listen to changes on the state
-  // 4. Update the state
+    // The Store should have four parts:
+    // Public facing API for our Store is the following:
+    // you can interact with the state by invoking one of the following methods
+    // 1. The state
+    // 2. Get state.
+    // 3. Listen to changes on the state
+    // 4. Update the state
 
-  let state
+    // the above invocaiton will give us the store!
 
+    let state;
 
-  const getState = () => state
+    let listeners = [];
 
-  return {
-    // the way a users can get the state is by making a store and then calling the getState()
-    getState
-  }
+    const getState = () => state;
 
+    const subscribe = listener => {
+        listeners.push(listener);
+        return () => (listeners = listeners.filter(l => l !== listener));
+    };
+
+    return {
+        // the way a users can get the state is by making a store and then calling the getState()
+        getState,
+        subscribe
+    };
 }
