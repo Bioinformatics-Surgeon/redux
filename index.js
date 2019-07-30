8a98fcb498b71b20f7f4a7bda082336b7a9708d9
Cody Goss <c.ward.goss@gmail.com>
2019-07-29 19:09:06 -0800

    add more notes for redux

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Actions DESCRIBE! the only things that can change the state in the store
+// Actions DESCRIBES! the only things that can change the state in the store.
 // - we only have to specifiy the actions TYPE; everything else is whatever we want
 
 const actionAddTodo = {
@@ -35,6 +35,51 @@ const actionToggleGoal = {
     id: 0
 };
 
+/*
+  ===== Pure Functions =====
+Characteristics of a Pure Function
+1) They always return the same output if recieving the same inputs
+    - if I call this over and over they will return the same return 
+2) They only depend on argumentss passed into them
+    - the never need variables outside of them 
+    - the reutrn is only depending on what is passed in
+3) They never produce any side effects
+    - no AJAX
+    - no mutation of state
+    - no messesing with DOM
+*/
+
+/*
+Reducer
+- must be a pure function 
+- it will take in the current state of our store and return the new state based on the action that was passed in
+
+-- the purpose of a reducer is to update that state based on an action occures
+
+1) takes in current state
+2) takes in specific action
+
+3) returns the new state based on action 
+
+-- this is sloved by using a PURE FUNCTION
+
+important take aways:
+1) the need to be pure
+2) they need to return the next state of the store
+3) they are responsible for setting the initial state of the store
+
+*/
+
+function todos(state = [], action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat([action.todo]);
+    } else if (action.type === 'REMOVE_TODO') {
+        return state.filter(todo => todo.id !== action.id);
+    }
+
+    return state;
+}
+
 function createStore() {
     // The Store should have four parts:
     // Public facing API for our Store is the following:
@@ -57,9 +102,31 @@ function createStore() {
         return () => (listeners = listeners.filter(l => l !== listener));
     };
 
+    const dispatch = action => {
+        // probably want to call out reducer todo function
+
+        state = todos(state, action);
+        // loop over all listeners and invoke them
+
+        listeners.forEach(listener => listener());
+    };
+
     return {
         // the way a users can get the state is by making a store and then calling the getState()
         getState,
-        subscribe
+        subscribe,
+        dispatch
     };
 }
+
+// From a users stand point
+
+const store = createStore();
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false
+    }
+});
